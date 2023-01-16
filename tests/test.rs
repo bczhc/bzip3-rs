7f06b0b7aac7cc5325ce2ebd230f8ee8c60ef33b
bczhc <bczhc0@126.com>
2023-01-16 21:02:48 +0800

    add tests for write-based bzip3 processors

--- tests/test.rs
+++ tests/test.rs
@@ -1,15 +1,14 @@
 use std::io;
-use std::io::{Cursor, Read, Write};
+use std::io::Cursor;
 
 use bytesize::ByteSize;
-use rand::RngCore;
+use rand::{thread_rng, RngCore};
 
-use bzip3::read::{Bz3Decoder, Bz3Encoder};
+use bzip3::{read, write};
 
 #[test]
 fn test() {
     println!("Test");
-    let mut rng = rand::thread_rng();
     let test_size_array = [
         0_usize,
         1,
@@ -35,40 +34,61 @@ fn test() {
 
     for data_size in test_size_array {
         for block_size in block_size_array {
-            let mut data = vec![0_u8; data_size];
-            rng.fill_bytes(&mut data);
-
-            let mut compressed = Cursor::new(Vec::new());
-            {
-                println!("encode: {:?}", (data_size, block_size));
-                let mut reader = Cursor::new(&mut data);
-                let mut encoder = Bz3Encoder::new(&mut reader, block_size).unwrap();
-                io_generic_copy(&mut encoder, &mut compressed).unwrap();
-            }
-            let compressed = compressed.into_inner();
-
-            let mut uncompressed = Cursor::new(Vec::new());
-            {
-                println!("decode: {:?}", (data_size, block_size));
-                let mut reader = Cursor::new(compressed);
-                let mut decoder = Bz3Decoder::new(&mut reader).unwrap();
-                assert_eq!(decoder.block_size() as usize, block_size);
-                io_generic_copy(&mut decoder, &mut uncompressed).unwrap();
-            }
-
-            assert_eq!(uncompressed.get_ref().as_slice(), data.as_slice());
+            test_read_based(data_size, block_size);
+            test_write_based(data_size, block_size);
         }
     }
 }
 
-fn io_generic_copy<R: Read, W: Write>(src: &mut R, dst: &mut W) -> io::Result<()> {
-    let mut buf = [0_u8; 4096];
-    loop {
-        let read_size = src.read(&mut buf)?;
-        if read_size == 0 {
-            break;
-        }
-        dst.write_all(&buf[..read_size])?;
+fn test_write_based(data_size: usize, block_size: usize) {
+    let data = generate_random_data(data_size);
+    let mut reader = Cursor::new(&data);
+    let mut writer = Cursor::new(Vec::new());
+
+    let mut encoder = write::Bz3Encoder::new(&mut writer, block_size).unwrap();
+    io::copy(&mut reader, &mut encoder).unwrap();
+    drop(encoder);
+
+    let compressed = writer.into_inner();
+
+    let mut reader = Cursor::new(compressed);
+    let mut writer = Cursor::new(Vec::new());
+
+    let mut decoder = write::Bz3Decoder::new(&mut writer);
+    io::copy(&mut reader, &mut decoder).unwrap();
+    drop(decoder);
+
+    assert_eq!(writer.into_inner(), data);
+}
+
+fn test_read_based(data_size: usize, block_size: usize) {
+    let mut data = generate_random_data(data_size);
+
+    let mut compressed = Cursor::new(Vec::new());
+    {
+        println!("encode: {:?}", (data_size, block_size));
+        let mut reader = Cursor::new(&mut data);
+        let mut encoder = read::Bz3Encoder::new(&mut reader, block_size).unwrap();
+        io::copy(&mut encoder, &mut compressed).unwrap();
     }
-    Ok(())
+    let compressed = compressed.into_inner();
+
+    let mut uncompressed = Cursor::new(Vec::new());
+    {
+        println!("decode: {:?}", (data_size, block_size));
+        let mut reader = Cursor::new(compressed);
+        let mut decoder = read::Bz3Decoder::new(&mut reader).unwrap();
+        assert_eq!(decoder.block_size() as usize, block_size);
+        io::copy(&mut decoder, &mut uncompressed).unwrap();
+    }
+
+    assert_eq!(uncompressed.get_ref().as_slice(), data.as_slice());
+}
+
+fn generate_random_data(size: usize) -> Vec<u8> {
+    let mut rng = thread_rng();
+
+    let mut data = vec![0_u8; size];
+    rng.fill_bytes(&mut data);
+    data
 }
