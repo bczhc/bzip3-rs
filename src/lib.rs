ff49ca48f43842cf88e4c9bacb29f17613b8d2cf
bczhc <bczhc0@126.com>
2023-06-11 22:58:20 +0800

    expose `crate::{Error, Result}`
    just link the two types from `crate::errors` to
    the crate root

--- src/lib.rs
+++ src/lib.rs
@@ -38,13 +38,14 @@ pub mod errors;
 pub mod read;
 pub mod stream;
 pub mod write;
+pub use errors::{Error, Result};
 
 /// The signature of a bzip3 file.
 pub const MAGIC_NUMBER: &[u8; 5] = b"BZ3v1";
 
-pub(crate) fn check_block_size(block_size: usize) -> Result<(), ()> {
+pub(crate) fn check_block_size(block_size: usize) -> Result<()> {
     if block_size < ByteSize::kib(65).0 as usize || block_size > ByteSize::mib(511).0 as usize {
-        Err(())
+        Err(Error::BlockSize)
     } else {
         Ok(())
     }
